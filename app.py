a8ca53f61facb7557f8796cd17955367e1f9bb79
kelvi <patelkelvi23@gmail.com>
2025-07-03 14:58:18 +0530

    new app.py file changes

--- app.py
+++ app.py
@@ -22,11 +22,14 @@ def predict_api():
     print(output[0])
     return jsonify(output=output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print("final input",final_input)
+    output=model.predict(final_input)[0]
+    print(output)
+    return render_template("home.html",prediction_txt="the predicted price is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
-
-
-
-
-
-

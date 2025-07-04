f261933ecb8a8c9efcb93be756e00289feb69824
kelvi <patelkelvi23@gmail.com>
2025-07-04 18:28:32 +0530

    finaL21

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def predict():
     return render_template("home.html",prediction_txt="The price is {}".format(output))
 
 if __name__=="__main__":
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+

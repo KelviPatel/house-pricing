72cca327665386b83f569025ebe4014153547339
kelvi <patelkelvi23@gmail.com>
2025-07-04 17:37:11 +0530

    finaL11

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-# model loading
+# model 
 model=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 
